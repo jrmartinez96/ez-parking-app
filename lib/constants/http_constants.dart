e146e8cefd7f3cf2ab5ee23bc6e97ca7ac134a06
Pepe Martinez <pepemart1996@gmail.com>
2021-08-14 22:32:13 -0600

    endpoint de ambiente develop

--- lib/constants/http_constants.dart
+++ lib/constants/http_constants.dart
@@ -1,1 +1,5 @@
-const urlEndpoint = 'http://10.203.1.139:8000/api';
+// Ambiente local
+// const urlEndpoint = 'http://10.203.1.139:8000/api';
+
+// Ambiente develop
+const urlEndpoint = 'https://ez-parking-develop.herokuapp.com/api';
