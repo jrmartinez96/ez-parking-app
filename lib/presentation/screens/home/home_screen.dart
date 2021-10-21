e649e99ef7f7ec0b9977d003306ae13ac482325b
Pepe Martinez <pepemart1996@gmail.com>
2021-10-21 08:06:11 -0600

    home tutorial

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -11,6 +11,8 @@ import 'package:flutter_svg/svg.dart';
 import 'package:ez_parking_app/core/utils/utils.dart' as utils;
 import 'package:nfc_manager/nfc_manager.dart';
 import 'package:nfc_manager/platform_tags.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tutorial/tutorial.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -20,6 +22,80 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final settingsKey = GlobalKey();
+  final transactionsKey = GlobalKey();
+  final nfcButtonKey = GlobalKey();
+
+  List<TutorialItens> tutorialItems = [];
+
+  @override
+  void initState() {
+    tutorialItems.addAll({
+      TutorialItens(
+          globalKey: settingsKey,
+          touchScreen: true,
+          top: 200,
+          left: 50,
+          children: [
+            const Text(
+              'Presiona aqui para ver tus configuraciones y administrar tus tarjetas de crédito.',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 100,
+            )
+          ],
+          widgetNext: _buildContinueTutorialButton('Presiona aqui para continuar'),
+          shapeFocus: ShapeFocus.oval),
+      TutorialItens(
+        globalKey: transactionsKey,
+        touchScreen: true,
+        top: 200,
+        left: 50,
+        children: [
+          const Text(
+            'Presiona aqui para poder ver tu historial de transacciones, incluyendo las actuales.',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          const SizedBox(
+            height: 100,
+          )
+        ],
+        widgetNext: _buildContinueTutorialButton('Presiona aqui para continuar'),
+        shapeFocus: ShapeFocus.oval,
+      ),
+      TutorialItens(
+        globalKey: nfcButtonKey,
+        touchScreen: true,
+        top: 50,
+        left: 50,
+        children: [
+          const Text(
+            'Presiona aqui y acerca tu telefono al sensor para poder abrir la talanquera.',
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+          const SizedBox(
+            height: 10,
+          )
+        ],
+        widgetNext: Container(),
+        shapeFocus: ShapeFocus.square,
+      ),
+    });
+    super.initState();
+    Future.delayed(Duration.zero, () async {
+      final sharedPreferences = await SharedPreferences.getInstance();
+
+      final firstTimeHome = sharedPreferences.getBool('first_time_home');
+
+      if (firstTimeHome == null || !firstTimeHome) {
+        Tutorial.showTutorial(context, tutorialItems);
+        await sharedPreferences.setBool('first_time_home', true);
+      }
+    });
+  }
+
   Future<void> _readNfcTag(BuildContext context) async {
     // Check availability
     final isAvailable = await NfcManager.instance.isAvailable();
@@ -37,8 +113,6 @@ class _HomeScreenState extends State<HomeScreen> {
             await NfcManager.instance.stopSession();
           }
 
-          final ndef = Ndef.from(tag);
-
           var id = '';
 
           if (Platform.isIOS) {
@@ -48,7 +122,6 @@ class _HomeScreenState extends State<HomeScreen> {
             final nfcA = NfcA.from(tag);
             id = nfcA!.identifier.map((e) => e.toRadixString(16).padLeft(2, '0')).join();
           }
-          print(id);
 
           // Llamada para abrir talanquera
           utils.showLoading(context);
@@ -71,6 +144,7 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: primary,
         centerTitle: true,
         leading: IconButton(
+          key: transactionsKey,
           icon: const Icon(
             Icons.history,
             size: 40,
@@ -82,6 +156,7 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         actions: [
           IconButton(
+            key: settingsKey,
             icon: const Icon(Icons.settings, size: 35, color: Colors.white),
             onPressed: () => Navigator.of(context).pushNamed('/settings'),
           ),
@@ -159,6 +234,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: Container(
                   margin: const EdgeInsets.all(20),
                   child: InkWell(
+                    key: nfcButtonKey,
                     onTap: () => _readNfcTag(context),
                     customBorder: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20),
@@ -184,6 +260,24 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           );
         },
+      ),
+    );
+  }
+
+  Widget _buildContinueTutorialButton(String title) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        title,
+        style: const TextStyle(
+          color: primary,
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+        ),
       ),
     );
   }
